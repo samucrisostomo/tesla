a1e9e6368f4bb0732107754f2c32088a6bd2e2e8
samuel <samolivercris@gmail.com>
2024-08-14 21:09:23 -0300

    primeiro commit

---
+++ scripts.js
@@ -0,0 +1,9 @@
+let formulario = document.querySelector(".formulario")
+
+function aparecerFormulario() {
+    formulario.style.left = "30%"
+}
+
+function desaparecerFormulario() {
+    formulario.style.left = "-50%"
+}
